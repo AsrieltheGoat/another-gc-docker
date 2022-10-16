b6230f3abf4fc25d2e6b090a55a4183c775aeab3
Joshua Noakes <joshua.noakes1@icloud.com>
2022-10-16 19:11:11 +0100

    move to alpine builds to save like 200% build time

---
+++ server/alpine/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+apk update
+#apk add
+apk add --no-cache git curl wget openjdk17-jre-headless
+mkdir ./grass
+cd ./grass
+rm -rf ./grasscutter*.*jar
+curl -s $GC_RELEASE | grep grasscutter*.*jar | cut -d '"' -f 4 | wget -qi -
+mkdir ../res-repo
+cd ../res-repo
+git clone $GC_RES_REPO
+cd ../grass
+ln -s ../res-repo/Grasscutter_Resources/Resources ./resources
+
