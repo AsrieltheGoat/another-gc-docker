55e7f8bdec017277a1629ee40766281b2cfb32b4
Joshua Noakes <joshua.noakes1@icloud.com>
2022-10-20 20:14:18 +0100

    added fix for using alt resources

--- server/build/downloadResources.sh
+++ server/build/downloadResources.sh
@@ -1,7 +1,15 @@
 #!/bin/ash
+# Check USE_ALT_RES_REPO and replace with alternative repo if set
+if [ "$USE_ALT_RES_REPO" = "true" ]; then
+    echo "Using alternative resource repository..."
+    GC_RES_REPO_DL="$GC_RES_REPO_ALT"
+else
+    GC_RES_REPO_DL="$GC_RES_REPO"
+fi
+
 mkdir -p /root/grass/res-repo
 cd /root/grass/res-repo
-git clone $GC_RES_REPO
+echo $GC_RES_REPO_DL
+git clone $GC_RES_REPO_DL Grasscutter_Resources
 cd /root/grass
 ln -s /root/grass/res-repo/Grasscutter_Resources/Resources ./resources
-
